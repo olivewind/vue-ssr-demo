367a1f0aa3c5f1a248d91ea28e1903679d3c4e70
olivewind <olive@olivewind.com>
2018-03-07 10:59:35 +0800

    core(*): init

---
+++ src/config/category.js
@@ -0,0 +1,14 @@
+module.exports =  [
+    {
+        "id": "5562b410e4b00c57d9b94a92",
+        "name": "Android",
+        "title": "android",
+        "tagId": "5597838ee4b08a686ce2319d",
+    },
+    {
+        "id": "5562b415e4b00c57d9b94ac8",
+        "name": "前端",
+        "title": "frontend",
+        "tagId": "5597a05ae4b08a686ce56f6f",
+    }
+]
